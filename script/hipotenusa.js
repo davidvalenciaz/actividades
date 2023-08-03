0ae13324350a90c5e620c836d8c2637269ce2b0f
davidvalenciaz <mkcallese@gmail.com>
2023-08-03 15:52:43 -0500

    actividadesjs

---
+++ script/hipotenusa.js
@@ -0,0 +1,6 @@
+let cateto1 = prompt("ingrese el valor del primero cateto")
+let cateto2 = prompt("ingrese el valor del segundo  cateto")
+
+let h = Math.sqrt(cateto1 **2 + cateto2 **2) 
+
+console.log("la hiponenusa es "+ h)
